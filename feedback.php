7fb72dbcd8e5fb943934d89982a21c5d9926a41d
Taek Been Nam <taekbeen93@gmail.com>
2023-12-11 13:08:51 -0500

    clean up

--- feedback.php
+++ feedback.php
@@ -1,29 +1,6 @@
 <?php include 'inc/header.php'; ?>
 
 <?php
-  // $feedback = [
-  //   [
-  //     'id' => '1',
-  //     'name' => 'Taek Nam',
-  //     'email' => 'taekbeen30@gmail.com',
-  //     'body' => 'I\'m a front end developer'
-  //   ],
-  //   [
-  //     'id' => '2',
-  //     'name' => 'George Sand',
-  //     'email' => 'gsand@gmail.com',
-  //     'body' => 'Simplicity is the most difficult thing to secure in this world; it is the last limit of experience and the last effort of genius.'
-  //   ],
-  //   [
-  //     'id' => '3',
-  //     'name' => 'St. Francis de Sales',
-  //     'email' => 'sfdsales@gmail.com',
-  //     'body' => 'True progress quietly and persistently moves along without notice.'
-  //   ],
-  // ]
-?>
-
-<?php
   $sql = 'SELECT * FROM feedback';
   $result = mysqli_query($conn, $sql);
   $feedback = mysqli_fetch_all($result, MYSQLI_ASSOC);
